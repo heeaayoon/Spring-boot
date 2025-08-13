2458b7616b52c0606240387c1639a7689782b515
Heeaayoon <soohe14@gmail.com>
2025-08-13 15:50:36 +0900

    Chapter061 DB연동 준비

---
+++ Chapter061/src/main/java/com/rubypaper/service/BoardServiceImpl.java
@@ -0,0 +1,42 @@
+package com.rubypaper.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rubypaper.domain.Board;
+import com.rubypaper.persistence.BoardRepository;
+
+@Service //빈 객체 생성
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardRepository boardRepo;
+
+	@Override
+	public List<Board> getBoardList() {
+		return boardRepo.findAll();
+	}
+
+	@Override
+	public Board getBoard(Board board) {
+		return null;
+	}
+
+	@Override
+	public void insertBoard(Board board) {
+		//boardRepo.
+	}
+
+	@Override
+	public void updateBoard(Board board) {
+		
+	}
+
+	@Override
+	public void deleteBoard(Board board) {
+		
+	}
+
+}
